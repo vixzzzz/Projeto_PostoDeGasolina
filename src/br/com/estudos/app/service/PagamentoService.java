41be2e0c9e04b44baafc1cf4def888378ac8e807
vixzzzz <victoremanuel1485@gmail.com>
2026-03-04 11:01:01 -0300

    feat: implementado sistema de pagamento com descontos para PIX, Dinheiro e cartao

---
+++ src/br/com/estudos/app/service/PagamentoService.java
@@ -0,0 +1,35 @@
+package br.com.estudos.app.service;
+
+import javax.swing.*;
+
+public class PagamanetoService {
+
+    private double valor;
+    private String op;
+
+    public void pagar(double valor) {
+
+        AbastecimentoService valortotal = new AbastecimentoService();
+
+        this.valor = valor;
+
+        if (valor < valortotal.getTotal()) {
+            JOptionPane.showMessageDialog(null, "Valor insuficiente");
+        } else if (op.equalsIgnoreCase("PIX")) {
+            valor *= 0.90;
+            JOptionPane.showMessageDialog(null, "Desconto de 10% aplicado no pix!");
+        } else if (op.equalsIgnoreCase("DINHEIRO")) {
+            valor *= 0.98;
+            JOptionPane.showMessageDialog(null, "Desconto de 2% aplicado no Dinheiro");
+        } else if (op.equalsIgnoreCase("CARTAO")) {
+            JOptionPane.showMessageDialog(null, "O combustivel esta pago!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Metodo invalido!");
+        }
+    }
+
+        public void opcoesDeP (String op) {
+            this.op = op;
+        }
+    }
+
